a0067d997b02b27fff2cdd050dc4bcda18c7c61b
Franklin Benjamin <franciscovibes14@gmail.com>
2022-04-13 23:55:22 -0400

    commit tree abaec2343ae26c19b5f1da4225f4d4f2f4a41cdc

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(tasks = "go to the office"){
+    return `This Monday, I will ${tasks}.`;
+}
+
+function wrapAdjective(symbol){
+    return function result(work){
+        return `You are ${symbol}${work}${symbol}!`;       
+    }
+}
